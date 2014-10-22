712d02db22b93962d2eb1cbc88677ac805a42756
Moz Morris <moz@earthview.co.uk>
2014-10-22 16:23:48 +0100

    CNAME.

--- gulpfile.js
+++ gulpfile.js
@@ -65,7 +65,7 @@ gulp.task('fonts', function () {
 });
 
 gulp.task('extras', function () {
-    return gulp.src(['app/*.*', '!app/*.html'], { dot: true })
+    return gulp.src(['app/*.*', '!app/*.html', 'app/CNAME'], { dot: true })
         .pipe(gulp.dest('dist'));
 });
 
